ec8add55e5afcc18dc821b02b0c1ec6c2e0f3439
mdre797 <markdrecoll@gmail.com>
2021-06-03 14:42:23 -0500

    Made api call parameters variable.

--- routes/api/weatherApi.js
+++ routes/api/weatherApi.js
@@ -1,12 +1,11 @@
 const router = require("express").Router();
 const Axios = require('axios')
 
-
+const weatherParamsToPassIn = "waterTemperature,waveHeight"
 
 router.get('/', function(req,res){
-    Axios.get('https://api.stormglass.io/v2/weather/point?lat=41.9&lng=-87.6&params=waveHeight', {headers: {Authorization: '343ae392-c30b-11eb-9f40-0242ac130002-343ae496-c30b-11eb-9f40-0242ac130002'}})
+    Axios.get(`https://api.stormglass.io/v2/weather/point?lat=41.9&lng=-87.6&params=${weatherParamsToPassIn}`, {headers: {Authorization: '343ae392-c30b-11eb-9f40-0242ac130002-343ae496-c30b-11eb-9f40-0242ac130002'}})
     .then(data => {
-        // console.log(data);
         res.json(data.data);
     })
 })

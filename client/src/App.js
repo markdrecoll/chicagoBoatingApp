d28646cc94418eacb4e42a46f69d66e3ec47a601
Levi Kane <oneleggedlevi09@gmail.com>
2021-06-09 13:51:54 -0500

    links are linking properly

--- client/src/App.js
+++ client/src/App.js
@@ -1,4 +1,4 @@
-import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
+import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
 import WeatherList from './pages/WeatherList';
 import React, { useEffect, useState } from 'react';
 import Login from './components/Login';
@@ -8,64 +8,56 @@ import Landing from './pages/Landing';
 import Navbar from './components/NavBar';
 
 function App() {
+  const [loginState, setLoginState] = useState(false);
 
-    const [loginState, setLoginState] = useState(false)
+  useEffect(() => {
+    Axios.get('/api/user/loginCheck', { withCredentials: true }).then(function (
+      loginCheck
+    ) {
+      console.log('login check!!!', loginCheck);
+      setLoginState(loginCheck.data.logged_in);
+    });
+  }, []);
 
-    useEffect(() => {
+  console.log('LOGIN STATE ', loginState);
 
-        Axios.get('/api/user/loginCheck', { withCredentials: true }).then(function (loginCheck) {
-            console.log('login check!!!', loginCheck)
-            setLoginState(loginCheck.data.logged_in)
-        })
-    }, [])
+  return (
+    <Router>
+      <div className="container">
+        <div className="row">
+          <div className="col-12">
+            <Navbar setLoginState={setLoginState} login={loginState} />
 
-    console.log('LOGIN STATE ', loginState)
-
-    return (
-        <Router>
-            <div className="container">
-                <div className="row">
-
-                    <div className="col-12">
-                    <Navbar setLoginState={setLoginState} login={loginState} />
-
-                        {!loginState ? (
-                            <Switch>
-                                <Route exact path={"/"}>
-                                    <Landing />
-                                </Route>
-                                <Route exact path={"/login"}>
-                                    <Login setLoginState={setLoginState} />
-                                </Route>
-                                <Route exact path={"/harbor"}>
-                                    <Harbor />
-                                </Route>
-
-
-                            </Switch>
-
-
-                        ) : (
-                            <Switch>
-                                <Route exact path={"/"}>
-                                    <Landing />
-                                </Route>
-                                <Route exact path={"/weatherlist"}>
-                                    <WeatherList />
-                                </Route>
-                                <Route exact path={"/harbor"}>
-                                    <Harbor />
-                                </Route>
-                            </Switch>
-                        )}
-
-
-
-                    </div>
-                </div>
-            </div>
-        </Router>
-    )
+            {!loginState ? (
+              <Switch>
+                <Route exact path={'/'}>
+                  <Landing />
+                </Route>
+                <Route exact path={'/login'}>
+                  <Login setLoginState={setLoginState} />
+                </Route>
+                <Route exact path={'/harbor'}>
+                  <Harbor />
+                </Route>
+              </Switch>
+            ) : (
+              <Switch>
+                <Route exact path={'/'}>
+                  <Landing />
+                </Route>
+                <Route exact path={'/weatherlist'}>
+                  <WeatherList />
+                </Route>
+                <Route exact path={'/harbor'}>
+                  <Harbor />
+                </Route>
+              </Switch>
+            )}
+          </div>
+        </div>
+      </div>
+    </Router>
+  );
 }
 
 export default App;

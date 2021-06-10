f96b5785901f4c7ab3ffd62826696432ff51145c
harrisonblackburn <blackbhr@yahoo.com>
2021-06-10 10:31:23 -0500

    carousel working on landing

--- client/src/pages/Landing.js
+++ client/src/pages/Landing.js
@@ -4,21 +4,21 @@ import { BrowserRouter as Router, Route, Switch, Link } from "react-router-dom";
 function Landing() {
     return (
         <>
-        <div class="jumbotron">
-            <h1 class="display-4">Welcome to the Chicago Boating App</h1>
-            <hr class="my-4" />
+        <div className="jumbotron">
+            <h1 className="display-4">Welcome to the Chicago Boating App</h1>
+            <hr className="my-4" />
             <p>Sign Up or Log In to plan your next Lake Michigan adventure!</p>
         </div>
-         <div  class="carousel slide" data-ride="carousel">
-        <div class="carousel-inner">
-          <div class="carousel-item active">
-              <img src="https://media.tacdn.com/media/attractions-splice-spp-674x446/06/6e/b7/df.jpg" class="d-block w-100" alt="..."/>
+         <div  id="myCarousel" className="carousel slide" data-ride="carousel" data-bs-ride="carousel" data-bs-interval="3000">
+        <div className="carousel-inner">
+          <div className="carousel-item active">
+              <img src="https://media.tacdn.com/media/attractions-splice-spp-674x446/06/6e/b7/df.jpg" className="d-block w-100" alt="..."/>
           </div>
-          <div class="carousel-item active">
-            <img src="https://cache.desktopnexus.com/thumbseg/2352/2352281-bigthumbnail.jpg" class="d-block w-100" alt="..."/>
+          <div className="carousel-item">
+            <img src="https://cache.desktopnexus.com/thumbseg/2352/2352281-bigthumbnail.jpg" className="d-block w-100" alt="..."/>
           </div>
-          <div class="carousel-item active">
-            <img src="https://pbs.twimg.com/media/D-QUJazWkAAwdeZ.jpg" class="d-block w-100" alt="..."/>
+          <div className="carousel-item">
+            <img src="https://pbs.twimg.com/media/D-QUJazWkAAwdeZ.jpg" className="d-block w-100" alt="..."/>
           </div>
         </div>
       </div>

079ed28703e581893ff92250f86dc36572dceba5
mdre797 <markdrecoll@gmail.com>
2021-06-11 11:17:31 -0500

    Added openweather API call.

--- routes/api/regularWeatherApi.js
+++ routes/api/regularWeatherApi.js
@@ -2,9 +2,10 @@ const router = require("express").Router();
 const axios = require('axios')
 require('dotenv').config()
 
+const openWeatherApiKey = process.env.apiKey2
+
 router.get('/', function(req,res){
-    axios.get(`https://api.openweathermap.org/data/2.5/onecall?lat=${lat}&lon=${lng}&appid=b47cbc5dede4579f5550da5d2bb84dc3`,
-    {headers: {Authorization: process.env.apiKey}})
+    axios.get(`https://api.openweathermap.org/data/2.5/onecall?lat=41.9&lon=-87.6&appid=${openWeatherApiKey}`)
     .then(data => {
         res.json(data.data);
     }).catch(error => {

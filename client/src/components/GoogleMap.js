8fe552c65962588884554182705faf98deb8ae29
mdre797 <markdrecoll@gmail.com>
2021-06-13 16:42:03 -0500

    Harbors now marked on the google map. Removed duplicate address and email fields from harbor JSON. Removed fax numbers.

--- client/src/components/GoogleMap.js
+++ client/src/components/GoogleMap.js
@@ -11,6 +11,9 @@ class SimpleMap extends Component {
     },
     zoom: 11
   };
+
+// lat: 41.75,
+// lng: -88.15
  
   render() {
     return (
@@ -22,11 +25,52 @@ class SimpleMap extends Component {
           defaultCenter={this.props.center}
           defaultZoom={this.props.zoom}
         >
-            
           <AnyReactComponent
-            lat={41.87}
-            lng={-87.62}
-            text="Chicago"
+            lat={41.944}
+            lng={-87.638}
+            text="Belmont"
+          />
+
+        <AnyReactComponent
+            lat={41.960}
+            lng={-87.642}
+            text="Montrose"
+          />
+
+        <AnyReactComponent
+            lat={41.9284}
+            lng={-87.6342}
+            text="Diversey"
+          />
+        <AnyReactComponent
+            lat={41.8368}
+            lng={-87.6065}
+            text="31st"
+          />
+          <AnyReactComponent
+            lat={41.8857}
+            lng={-87.6117}
+            text="DuSable"
+          />
+          <AnyReactComponent
+            lat={41.8769}
+            lng={-87.6132}
+            text="Monroe"
+          />
+          <AnyReactComponent
+            lat={41.8604}
+            lng={-87.6124}
+            text="Burnham"
+          />
+          <AnyReactComponent
+            lat={41.7884}
+            lng={-87.5790}
+            text="59th"
+          />
+           <AnyReactComponent
+            lat={41.7772}
+            lng={-87.5742}
+            text="Jackson"
           />
         </GoogleMapReact>
       </div>

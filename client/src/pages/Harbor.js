9d55bc90f0d603cb64a908e870ae72107ea64467
Levi Kane <oneleggedlevi09@gmail.com>
2021-06-13 08:47:44 -0500

    added the favorite highlight to the harbors

--- client/src/pages/Harbor.js
+++ client/src/pages/Harbor.js
@@ -1,22 +1,36 @@
-import React from 'react';
+import React, {useState, useEffect} from 'react';
 import harborData from '../harborData.json';
 import API from "../utils/api";
 import GoogleMap from "../components/GoogleMap"
+// import e from 'cors';
 
 function Harbor() {
+    const [favHarbor, setFavHarbor]=useState('')
+
+    useEffect(() => {
+        handleHarborSave();
+      }, []);
+const handleHarborSave=(harborName)=>{
+    API.setHarborForUser(harborName).then((data)=>{
+        console.log(data)
+        setFavHarbor(data.data.harbor)
+    })
+}
   return (
     <div className="container row justify-content-center">
-      <h1 className="col-12">Harbor Information</h1>
-      <p>Click on a harbor to set that as your designated harbor.</p>
+      <h1 className="col-12 bg-light p-5">Harbor Information</h1>
+      
 
 
       <GoogleMap />
-
-
-
+        <div className="bg-light p-5">
+        <h2>Choose your favorite Harbor</h2>
+      <h5>Save a Harbor as your designated harbor.</h5>
+        </div>
+        
       {harborData.map((harbor) => (
         // add on click ability to make api call to set default harbor for the user
-        <div className="card col-lg-3 mx-2 my-2" key={harbor.id} >
+        <div className="card col-lg-3 mx-2 my-2 border border-dark" key={harbor.id} >
           <h2 className="card-header fluid">{harbor.name}</h2>
           <ul>
             <li>
@@ -36,7 +50,11 @@ function Harbor() {
               <button data-address={harbor.address}>Map</button>
             </li> */}
           </ul>
-          <button className="btn btn-primary col-md-6 mb-3" onClick={()=> API.setHarborForUser(harbor.name)}>Save as Favorite</button>
+          <div className="footer mt-auto pb-2 pt-3 bg-light justify-content-center">
+            <button className="btn btn-primary col-md-6 mb-2 me-2" onClick={()=>handleHarborSave(harbor.name)}>Save as Favorite</button>
+            {favHarbor === harbor.name?(<button className="btn btn-success mb-2">Favorite</button>):(<button className="btn btn-success d-none">Favorite</button>)}
+          </div>
+          
         </div>
       ))}
     </div>

280505470a5aeedfe098bf462a0160cf09ecc5de
mdre797 <markdrecoll@gmail.com>
2023-02-22 16:13:25 -0600

    Testing heroku fixes.

--- server.js
+++ server.js
@@ -19,7 +19,7 @@ app.use(express.json());
 // }
 
 if (process.env.NODE_ENV === "production") {
-    app.use(express.static("/client/build"));
+    app.use(express.static(__dirname, "/client/build"));
   }
 
 const sess = {

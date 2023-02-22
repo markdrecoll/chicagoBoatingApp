6e2f18e0c54a5a656767a5d806e6a4c9d132e5a3
mdre797 <markdrecoll@gmail.com>
2023-02-22 15:21:57 -0600

    Changed example component name.

--- client/src/components/GoogleMap.js
+++ client/src/components/GoogleMap.js
@@ -1,7 +1,7 @@
 import React, { Component } from 'react';
 import GoogleMapReact from 'google-map-react';
 
-const AnyReactComponent = ({ text }) => <div>{text}</div>;
+const HarborMapMarker = ({ text }) => <div>{text}</div>;
 
 class SimpleMap extends Component {
   static defaultProps = {
@@ -24,84 +24,81 @@ class SimpleMap extends Component {
           defaultCenter={this.props.center2}
           defaultZoom={this.props.zoom}
         >
-          <AnyReactComponent
+          <HarborMapMarker
             lat={41.944}
             lng={-87.638}
             text="Belmont"
           />
-          <AnyReactComponent
+          <HarborMapMarker
             lat={41.960}
             lng={-87.642}
             text="Montrose"
           />
-          <AnyReactComponent
+          <HarborMapMarker
             lat={41.9284}
             lng={-87.6342}
             text="Diversey"
           />
-          <AnyReactComponent
+          <HarborMapMarker
             lat={41.8368}
             lng={-87.6065}
             text="31st"
           />
-          <AnyReactComponent
+          <HarborMapMarker
             lat={41.8857}
             lng={-87.6117}
             text="DuSable"
           />
-          <AnyReactComponent
+          <HarborMapMarker
             lat={41.8769}
             lng={-87.6132}
             text="Monroe"
           />
-          <AnyReactComponent
+          <HarborMapMarker
             lat={41.8604}
             lng={-87.6124}
             text="Burnham"
           />
-          <AnyReactComponent
+          <HarborMapMarker
             lat={41.7884}
             lng={-87.5790}
             text="59th"
           />
-          <AnyReactComponent
+          <HarborMapMarker
             lat={41.7772}
             lng={-87.5742}
             text="Jackson"
           />
-          <AnyReactComponent
+          <HarborMapMarker
             lat={42.0778}
             lng={-87.6790}
             text="Wilmette"
           />
-          <AnyReactComponent
+          <HarborMapMarker
             lat={42.3589}
             lng={-87.8243}
             text="Waukegan"
           />
-          <AnyReactComponent
+          <HarborMapMarker
             lat={42.6723}
             lng={-87.8058}
             text="Kenosha"
           />
-          <AnyReactComponent
+          <HarborMapMarker
             lat={43.04510}
             lng={-87.8802}
             text=""
           />
-          <AnyReactComponent
+          <HarborMapMarker
             lat={41.7280}
             lng={-86.9129}
             text="Michigan City"
           />
-          <AnyReactComponent
+          <HarborMapMarker
             lat={41.7981}
             lng={-86.7447}
             text="New Buffalo"
           />
-
-
-
         </GoogleMapReact>
       </div>
     );
